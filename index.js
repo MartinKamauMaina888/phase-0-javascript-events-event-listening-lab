760beb1801b2dcf9dd57c9b251c855abf808003e
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-12-09 16:52:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 function addingEventListener() {
+
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
 }
+
+addingEventListener()
+
+// **********************************************Option 2 **********************************************
+
+// function clickMe() {
+//     return alert('I was clicked!')
+// }
+
+// function addingEventListener() {
+
+//     const input = document.getElementById('input');
+//     input.addEventListener('click', clickMe);
+
+// }
+
+// addingEventListener()
